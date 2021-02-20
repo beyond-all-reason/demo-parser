6fc01211a4e142017e98417f0e7275625f8f3c0f
Jazcash <jaspercashmore@gmail.com>
2021-02-20 13:57:21 +0000

    export utils

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import { DemoModel } from "./demo-model";
 import { DemoParser, DemoParserConfig } from "./demo-parser";
+import { isPlayer, isSpec, isPacket } from "./utils";
 
-export { DemoModel, DemoParser, DemoParserConfig };
+export { DemoModel, DemoParser, DemoParserConfig, isPlayer, isSpec, isPacket };
